f9e5bc701cc3638f953f5f754b72cac97e49646f
tinotringali <tringali.tino94@gmail.com>
2023-04-17 22:13:09 +0200

    fixed

--- models/products.php
+++ models/products.php
@@ -1,13 +1,13 @@
 <?php
 require_once __DIR__ . '/categories.php';
-class Product
+class Products
 {
     public $img;
     public $productName;
     public $price;
     public $category;
     public function  __construct(string $_img, string $_productName, float  $_price, string $_category)
-    
+
     {
         $this->img = $_img;
         $this->productName = $_productName;
